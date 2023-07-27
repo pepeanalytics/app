c4dc83a1c2d500940a6af3bd2b4c240460e9deb2
yoyobojo <angellolazar@gmail.com>
2023-07-27 09:27:09 -0400

    added css to know which sidebar tab you're on

--- src/components/SideBarIcon.jsx
+++ src/components/SideBarIcon.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function SideBarIcon({ icon, route }) {
+export default function SideBarIcon({ icon, route, current, routes }) {
   return (
-    <Link to={route} className="sidebar-icon">
-      <img src={icon} alt="icon" />
+    <Link to={route} className="sidebar-icon" style={{ boxShadow: routes.includes(current) ? "0px 0px 10px 0px rgba(238,51,125,1)" : "" }}>
+      <img 
+        src={icon} 
+        alt="icon" 
+      />
     </Link>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,40 +8,48 @@ import chartIcon from "../assets/images/chart.svg";
 import sniper from "../assets/images/sniper.svg";
 
 import SideBarIcon from "./SideBarIcon";
+import { useLocation } from "react-router-dom";
 
 const Icons = [
   {
     icon: homeIcon,
     link: "/",
+    links: [""]
   },
   {
     icon: frog,
     link: "/main",
+    links: ["/main"]
   },
   {
     icon: magnifierIcon,
     link: "/search-token-sniffer",
+    links: ["/search-token-sniffer", "/token-sniffer"]
   },
   {
     icon: chartIcon,
     link: "/search-analysis",
+    links: ["/search-analysis", "/analysis"]
   },
   {
     icon: user,
     link: "/social",
+    links: ["/social", "/holders"]
   },
   {
     icon: sniper,
     link: "/sniper",
+    links: ["/sniper"]
   },
 ];
 
 export default function Sidebar() {
+  const { pathname } = useLocation();
   return (
     <div className="siderbarWrapper">
       <div className="sidebar">
-        {Icons.map(({ icon, link }, index) => {
-          return <SideBarIcon key={index} route={link} icon={icon} />;
+        {Icons.map(({ icon, link, links }, index) => {
+          return <SideBarIcon routes={links} key={index} route={link} icon={icon} current={pathname} />;
         })}
       </div>
     </div>
